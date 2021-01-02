d3c4c09e7dbd53a42d54d25201e4d070796a9082
krishnkumar774 <krishn.krishn2@gmail.com>
2021-01-02 22:10:38 +0530

    firstcase

---
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	@Test
+	public void deleteTwiter()
+	{
+		System.out.println("deleteTwiter");
+	}
+}
